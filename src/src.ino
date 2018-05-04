d983e99d8ac6bca82fadecf3ddf80a33b504519c
tuxsoft <info@tuxsoft.co.za>
2018-05-04 18:32:25 +0200

    Added OTA support

--- src/src.ino
+++ src/src.ino
@@ -44,6 +44,7 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <Wire.h>
 #include <time.h>
 #include <Adafruit_BMP280.h>
@@ -52,6 +53,8 @@
 #include "WeatherCock.h"
 #include "TuxSoft3DP.h"
 #include "WeatherFlow.h"
+
+#define OTA_ENABLE	1
 
 DHT dht (D5, DHT22);
 Adafruit_BMP280 bmp;	// I2C version
@@ -138,6 +141,30 @@ void setup()
 		Serial.print ("c.");
 	}
 
+#ifdef OTA_ENABLE
+
+	ArduinoOTA.setHostname ("weather");
+
+	ArduinoOTA.onStart ([](){ Serial.println("Start"); });
+	ArduinoOTA.onEnd ([]() { Serial.println("\nEnd"); });
+
+	ArduinoOTA.onProgress ([](unsigned int progress, unsigned int total)
+	{
+		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+	});
+	
+	ArduinoOTA.onError([](ota_error_t error)
+	{
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+		else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
+	ArduinoOTA.begin();
+
+#endif
 	getTime ();
 
 	// GMT+2
@@ -187,6 +214,10 @@ void loop()
 	static int windLSample = 0;
 	static int lmin = -1;
 	static int lsec = -1;
+
+#ifdef OTA_ENABLE
+	ArduinoOTA.handle();
+#endif
 
 	// Tux#DP parms
 	static float txWindGust = 0;
