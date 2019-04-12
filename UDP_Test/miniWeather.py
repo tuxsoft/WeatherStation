9c9ee7ced115c481d7e38e0064319e45e7baa499
ross <tnfo@tuxsoft.co.za>
2019-04-12 11:21:53 +0200

    Change to use timeouts to reduce CPU and simplify for OTA detection

--- UDP_Test/miniWeather.py
+++ UDP_Test/miniWeather.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # Multicast client
 # Adapted from: http://chaos.weblogs.us/archives/164
 # Compass from: https://wiki.python.org/moin/PyQt/Compass%20widget
@@ -9,6 +9,7 @@ from PyQt4.QtGui import *
 
 import socket
 import json
+import time
 
 ANY = "0.0.0.0" 
 MCAST_ADDR = "224.0.0.7"
@@ -16,101 +17,101 @@ MCAST_PORT = 55555
 
 class CompassWidget(QWidget):
 
-	angleChanged = pyqtSignal(float)
-	
-	def __init__(self, parent = None):
-	
-		QWidget.__init__(self, parent)
-		
-		self._angle = 0.0
-		self._margins = 10
-		self._pointText = {0: "N", 45: "NE", 90: "E", 135: "SE", 180: "S",
-						   225: "SW", 270: "W", 315: "NW"}
-	
-	def paintEvent(self, event):
-	
-		painter = QPainter()
-		painter.begin(self)
-		painter.setRenderHint(QPainter.Antialiasing)
-		
-		painter.fillRect(event.rect(), self.palette().brush(QPalette.Window))
-		self.drawMarkings(painter)
-		self.drawNeedle(painter)
-		
-		painter.end()
-	
-	def drawMarkings(self, painter):
-	
-		painter.save()
-		painter.translate(self.width()/2, self.height()/2)
-		scale = min((self.width() - self._margins)/120.0,
-					(self.height() - self._margins)/120.0)
-		painter.scale(scale, scale)
-		
-		font = QFont(self.font())
-		font.setPixelSize(10)
-		metrics = QFontMetricsF(font)
-		
-		painter.setFont(font)
-		painter.setPen(self.palette().color(QPalette.Shadow))
-		
-		i = 0
-		while i < 360:
-		
-			if i % 45 == 0:
-				painter.drawLine(0, -40, 0, -50)
-				painter.drawText(-metrics.width(self._pointText[i])/2.0, -52,
-								 self._pointText[i])
-			else:
-				painter.drawLine(0, -45, 0, -50)
-			
-			painter.rotate(15)
-			i += 15
-		
-		painter.restore()
-	
-	def drawNeedle(self, painter):
-	
-		painter.save()
-		painter.translate(self.width()/2, self.height()/2)
-		painter.rotate(self._angle)
-		scale = min((self.width() - self._margins)/120.0,
-					(self.height() - self._margins)/120.0)
-		painter.scale(scale, scale)
-		
-		painter.setPen(QPen(Qt.NoPen))
-		painter.setBrush(self.palette().brush(QPalette.Shadow))
-		
-		painter.drawPolygon(
-			QPolygon([QPoint(-5, 0), QPoint(0, -45), QPoint(5, 0),
-					  QPoint(0, 5), QPoint(-5, 0)])
-			)
-		
-		painter.setBrush(self.palette().brush(QPalette.Highlight))
-		
-		painter.drawPolygon(
-			QPolygon([QPoint(-5, -25), QPoint(0, -45), QPoint(5, -25),
-					  QPoint(0, -30), QPoint(-5, -25)])
-			)
-		
-		painter.restore()
-	
-	def sizeHint(self):
-	
-		return QSize(150, 150)
-	
-	def angle(self):
-		return self._angle
-	
-	@pyqtSlot(float)
-	def setAngle(self, angle):
-	
-		if angle != self._angle:
-			self._angle = angle
-			self.angleChanged.emit(angle)
-			self.update()
-	
-	angle = pyqtProperty(float, angle, setAngle)
+    angleChanged = pyqtSignal(float)
+    
+    def __init__(self, parent = None):
+    
+        QWidget.__init__(self, parent)
+        
+        self._angle = 0.0
+        self._margins = 10
+        self._pointText = {0: "N", 45: "NE", 90: "E", 135: "SE", 180: "S",
+                           225: "SW", 270: "W", 315: "NW"}
+    
+    def paintEvent(self, event):
+    
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        painter.fillRect(event.rect(), self.palette().brush(QPalette.Window))
+        self.drawMarkings(painter)
+        self.drawNeedle(painter)
+        
+        painter.end()
+    
+    def drawMarkings(self, painter):
+    
+        painter.save()
+        painter.translate(self.width()/2, self.height()/2)
+        scale = min((self.width() - self._margins)/120.0,
+                    (self.height() - self._margins)/120.0)
+        painter.scale(scale, scale)
+        
+        font = QFont(self.font())
+        font.setPixelSize(10)
+        metrics = QFontMetricsF(font)
+        
+        painter.setFont(font)
+        painter.setPen(self.palette().color(QPalette.Shadow))
+        
+        i = 0
+        while i < 360:
+        
+            if i % 45 == 0:
+                painter.drawLine(0, -40, 0, -50)
+                painter.drawText(-metrics.width(self._pointText[i])/2.0, -52,
+                                 self._pointText[i])
+            else:
+                painter.drawLine(0, -45, 0, -50)
+            
+            painter.rotate(15)
+            i += 15
+        
+        painter.restore()
+    
+    def drawNeedle(self, painter):
+    
+        painter.save()
+        painter.translate(self.width()/2, self.height()/2)
+        painter.rotate(self._angle)
+        scale = min((self.width() - self._margins)/120.0,
+                    (self.height() - self._margins)/120.0)
+        painter.scale(scale, scale)
+        
+        painter.setPen(QPen(Qt.NoPen))
+        painter.setBrush(self.palette().brush(QPalette.Shadow))
+        
+        painter.drawPolygon(
+            QPolygon([QPoint(-5, 0), QPoint(0, -45), QPoint(5, 0),
+                      QPoint(0, 5), QPoint(-5, 0)])
+            )
+        
+        painter.setBrush(self.palette().brush(QPalette.Highlight))
+        
+        painter.drawPolygon(
+            QPolygon([QPoint(-5, -25), QPoint(0, -45), QPoint(5, -25),
+                      QPoint(0, -30), QPoint(-5, -25)])
+            )
+        
+        painter.restore()
+    
+    def sizeHint(self):
+    
+        return QSize(150, 150)
+    
+    def angle(self):
+        return self._angle
+    
+    @pyqtSlot(float)
+    def setAngle(self, angle):
+    
+        if angle != self._angle:
+            self._angle = angle
+            self.angleChanged.emit(angle)
+            self.update()
+    
+    angle = pyqtProperty(float, angle, setAngle)
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -128,43 +129,49 @@ status = sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.ine
 
 # setblocking(0) is equiv to settimeout(0.0) which means we poll the socket.
 # But this will raise an error if recv() or send() can't immediately find or send data. 
-sock.setblocking(0)
+#sock.setblocking(0)
+sock.settimeout (1)
 
 if __name__ == "__main__":
 
-	app = QApplication(sys.argv)
-	
-	window = QWidget()
-	compass = CompassWidget()
-	layout = QVBoxLayout()
-	layout.addWidget(compass, 1)
-	speed = QLabel()
-	layout.addWidget(speed)
-	temp = QLabel()
-	layout.addWidget(temp)
+    app = QApplication(sys.argv)
+    
+    window = QWidget()
+    compass = CompassWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(compass, 1)
+    speed = QLabel()
+    layout.addWidget(speed)
+    temp = QLabel()
+    layout.addWidget(temp)
+    hum = QLabel()
+    layout.addWidget(hum)
 
-	window.setLayout(layout)
-	
-	window.show()
-	compass.setAngle (1)
-	app.processEvents ()
-	app.processEvents ()
-	app.processEvents ()
-	app.processEvents ()
+    window.setLayout(layout)
+    
+    window.show()
+    compass.setAngle (1)
+    app.processEvents ()
+    app.processEvents ()
+    app.processEvents ()
+    app.processEvents ()
 
-	while 1:
-		try:
-			app.processEvents()
-			data, addr = sock.recvfrom(1024)
-		except socket.error as e:
-			pass
-		else:
-			pkt = json.loads (data)
-			compass.setAngle (pkt["windDir"])
-			deg = u"\u00b0"
-			speed.setText (QString ("Wind: %1 kph (Max %2)").arg(pkt["windSpeed"]*3.6).arg(pkt["windGust"]*3.6))
-			temp.setText (QString ("Temp: %1%2C   Rain: %3 mm").arg(pkt["outTemp"]).arg(deg).arg(pkt["rain"]))
-			app.processEvents()
+    while 1:
+        try:
+            app.processEvents()
+            data, addr = sock.recvfrom(1024)
+        except socket.error as e:
+            pass
+        else:
+            if len(data) > 0 :
+                print time.strftime('%H:%M:%S : '), data
+                if 'TS3DP' in data:
+                    pkt = json.loads (data)
+                    compass.setAngle (pkt["windDir"])
+                    deg = u"\u00b0"
+                    speed.setText (QString ("Wind: %1 kph (Max %2)").arg(pkt["windSpeed"]*3.6).arg(pkt["windGust"]*3.6))
+                    temp.setText (QString ("Temp: %1%2C   Rain: %3 mm").arg(pkt["outTemp"]).arg(deg).arg(pkt["rain"]))
+                    hum.setText (QString ("Hum: %1%  BMP: %2hPa  UV:%3").arg(pkt["outHumidity"]).arg(pkt["barometer"]).arg(pkt["UV"]))
+            app.processEvents()
 
-
-	sys.exit(app.exec_())
+    sys.exit(app.exec_())
--- UDP_Test/udp_test.py
+++ UDP_Test/udp_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Multicast client
 # Adapted from: http://chaos.weblogs.us/archives/164
 
@@ -9,7 +9,7 @@ import json
 ANY = "0.0.0.0" 
 MCAST_ADDR = "224.0.0.7"
 MCAST_PORT = 55555
-WMCAST_PORT = 50222
+WMCAST_PORT = 55551
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -31,29 +31,31 @@ status = wsock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.in
 
 # setblocking(0) is equiv to settimeout(0.0) which means we poll the socket.
 # But this will raise an error if recv() or send() can't immediately find or send data. 
-sock.setblocking(0)
-wsock.setblocking(0)
+#sock.setblocking(0)
+#wsock.setblocking(0)
+# Rather use a timeout to reduce CPU usage
+sock.settimeout (30)
+sock.settimeout (30)
 
 while 1:
-	try:
-		data, addr = sock.recvfrom(1024)
-	except socket.error as e:
-		pass
-	else:
-		print "From: ", addr
-		print "TuxSoft3DP Data: ", data
-		pkt = json.loads (data)
-		print "outTemp", pkt["outTemp"];
-		print "windSpeed", pkt["windSpeed"];
-		print "windDir", pkt["windDir"];
-		print "windGust", pkt["windGust"];
+    try:
+        data, addr = sock.recvfrom(1024)
+    except socket.error as e:
+        pass
+    else:
+        if (len(data) > 0) :
+            print ("From: ", addr)
+            print ("TuxSoft3DP Data: ", data)
+            pkt = json.loads (data)
+            print ("outTemp", pkt["outTemp"])
+            print ("windSpeed", pkt["windSpeed"])
+            print ("windDir", pkt["windDir"])
+            print ("windGust", pkt["windGust"])
 
-
-
-	try:
-		data, addr = wsock.recvfrom(1024)
-	except socket.error as e:
-		pass
-	else:
-		print "From: ", addr
-		print "Weatherflow Data: ", data
+    try:
+        data, addr = wsock.recvfrom(1024)
+    except socket.error as e:
+        pass
+    else:
+        print ("From: ", addr)
+        print ("OTA Mode ? : ", data)
